ac8d1ba4d05c0c684809496de98d46e362a0841c
W0826070 <W0826070@ADL-A1201I-08>
2023-10-31 13:23:24 -0400

    added the new area under the Area folder name Customer and modify my statup.cs

--- VeerpalKaurBookStore/Startup.cs
+++ VeerpalKaurBookStore/Startup.cs
@@ -64,7 +64,7 @@ namespace VeerpalKaurBookStore
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
