bb9dd53fc3908f67d90f2bc00263f25a45db1fff
W0826070 <W0826070@ADL-A1201H-13>
2023-11-20 18:15:11 -0500

    then i added a index in view folder and modify my code... then i add a new js file in wwwroot named product.js...

--- VeerpalKaurBookStore/Areas/Admin/Controllers/ProductController.cs
+++ VeerpalKaurBookStore/Areas/Admin/Controllers/ProductController.cs
@@ -28,38 +28,38 @@ namespace VeerpalKaurBookStore.Areas.Admin.Controllers
         }
 
 
-        public IActionResult Upsert(int? id)
-        // get action method for Upsert
-        {
-            ProductVM productVM = new ProductVM()
-            {
-                Product = new Product(),
-                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
-                {
-                    Text = i.Name,
-                    Value = i.Id.ToString()
-                }),
+        //public IActionResult Upsert(int? id)
+        //// get action method for Upsert
+        //{
+        //    ProductVM productVM = new ProductVM()
+        //    {
+        //        Product = new Product(),
+        //        CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+        //        {
+        //            Text = i.Name,
+        //            Value = i.Id.ToString()
+        //        }),
 
-            CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
-            {
-                Text = i.Name,
-                Value = i.id.ToString()
-            }),
+        //    CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+        //    {
+        //        Text = i.Name,
+        //        Value = i.id.ToString()
+        //    }),
 
-            }; // using AndrewsBooks.Models;
-            if (id == null)
-            {
-                // this is for create
-                return View(productVM);
-            }
-            // this for the edit
-            productVM.Product = _unitOfWork.Product.Get(id.GetValueOrDefault());
-            if (productVM.Product == null)
-            {
-                return NotFound();
-            }
-            return View(productVM);
-        }
+        //    }; // using AndrewsBooks.Models;
+        //    if (id == null)
+        //    {
+        //        // this is for create
+        //        return View(productVM);
+        //    }
+        //    // this for the edit
+        //    productVM.Product = _unitOfWork.Product.Get(id.GetValueOrDefault());
+        //    if (productVM.Product == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(productVM);
+        //}
 
         //API calls here
 
